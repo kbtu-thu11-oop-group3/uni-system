ebca1f3825cfdef0f6586314fb38560478932a4d
aablty <acrouple@yandex.ru>
2026-05-20 02:07:32 +0500

    Remove Console UI

--- uni-system/src/main/java/com/kbtu/oop/project/ui/console/ConsoleApp.java
+++
@@ -1,9 +0,0 @@
-package com.kbtu.oop.project.ui.console;
-
-public class ConsoleApp {
-
-    public void start(String[] args) {
-        Menu.showWelcome();
-        new LoginController().start();
-    }
-}
